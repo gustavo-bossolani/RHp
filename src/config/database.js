ac1b1eaf0a55f0dcc2f2648b13705c413605e190
Lucas Barros <lucas2019.fullstack@gmail.com>
2020-03-26 22:52:03 -0300

    Deploy for Heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,15 @@
 require('dotenv').config()
 module.exports = {
-    dialect: 'postgres',
-    host: process.env.HOST || 'localhost',
-    username: process.env.DB_USERNAME || 'postgres',
-    password: process.env.DB_PASSWORD || 'docker',
-    database: process.env.DB_DATABASE || 'RHp',
-    define: {
-        timestamps: true,
-        underscored: true,
-        underscoredAll: true,
-    },
+    "staging": {
+        dialect: 'postgres',
+        host: process.env.HOST || 'localhost',
+        username: process.env.DB_USERNAME || 'postgres',
+        password: process.env.DB_PASSWORD || 'docker',
+        database: process.env.DB_DATABASE || 'RHp',
+        define: {
+            timestamps: true,
+            underscored: true,
+            underscoredAll: true,
+        },
+    }
 };
